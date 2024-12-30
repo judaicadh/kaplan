cd1cf9e2bfaf520019fa5db344e6b4365aaff230
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2024-12-30 17:43:22 -0500

    Subjects and Logo paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
-
-import preact from '@astrojs/preact'
-
-
 export default defineConfig({
-  site: "https://kaplancollection.org",
-
+  site: 'https://www.kaplancollection.org', // Update to your canonical URL
   prefetch: true,
   vite: {
     resolve: {
       alias: {
-        // alias icons to their ESM version
         '@mui/icons-material': '@mui/icons-material/esm',
       },
     },
@@ -26,20 +20,28 @@ export default defineConfig({
     optimizeDeps: {
       include: ['algoliasearch']
     },
-  }, // Close vite object here
-  base: "/", // For GitHub Pages, adjust if deploying to a subfolder
-
-  typescript: {
-    tsconfig: "./tsconfig.json", // Path to TypeScript config
   },
-
-  integrations: [react({
-    experimentalReactChildren: true // Enables experimental React children handling
-  }), tailwind(), icon({
-    include: {
-      // Include only three `mdi` icons in the bundle
-			mdi: ['magnify', 'account-plus', 'account-minus']
-      // Include all `uis` icons
-    }
-  }), sitemap(), robotsTxt()]
-});
+  base: '/', // Adjust for subfolder deployment if needed
+  typescript: {
+    tsconfig: './tsconfig.json'
+  },
+  integrations: [
+    react({ experimentalReactChildren: true }),
+    tailwind(),
+    icon({
+      include: {
+        mdi: ['magnify', 'account-plus', 'account-minus']
+      }
+    }),
+    sitemap(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/'
+        }
+      ],
+      sitemap: 'https://www.kaplancollection.org/sitemap.xml'
+    })
+  ]
+})
