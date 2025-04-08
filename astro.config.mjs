8911331f55f8c44aaa9d588176a9d4d808597950
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-04-08 17:31:56 -0400

    updated data

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,15 @@ import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import partytown from "@astrojs/partytown";
+
+import fs from "node:fs";
+import path from "node:path";
+
+// Read slugs from JSON file
+const itemsJsonPath = path.resolve("./src/data/items.json");
+const itemsData = JSON.parse(fs.readFileSync(itemsJsonPath, "utf-8"));
+const itemSlugs = itemsData.map((item) => `/item/${item.slug}`);
+
 export default defineConfig({
   site: 'https://www.kaplancollection.org', // Update to your canonical URL
   prefetch: true,
@@ -33,7 +42,14 @@ export default defineConfig({
     include: {
       mdi: ["magnify", "account-plus", "account-minus"]
     }
-  }), sitemap(), robotsTxt({
+  }), sitemap({
+    customPages: [
+      "/",
+      "/about",
+      ...itemSlugs // ← inject your dynamic routes here
+    ]
+  }),
+    robotsTxt({
     policy: [
       {
         userAgent: "*",
