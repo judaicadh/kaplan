13fbc9f7b00a375b70a844bafe2ba0e740fb5098
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-23 18:39:02 -0400

    updated cookies and load time

--- public/cookieconsent.js
+++ public/cookieconsent.js
@@ -1,5 +1,34 @@
 import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.1.0/dist/cookieconsent.umd.js';
 
 CookieConsent.run({
-  // your config. goes here (required)
+  categories: {
+    necessary: {
+      enabled: true,
+      readOnly: true
+    },
+    analytics: {
+      enabled: true // default
+    }
+  },
+  language: {
+    default: 'en',
+    translations: {
+      en: {
+        consentModal: {
+          title: 'We use cookies',
+          description: 'This website uses cookies to ensure you get the best experience.',
+          acceptAllBtn: 'Accept all',
+          acceptNecessaryBtn: 'Reject all',
+          showPreferencesBtn: 'Manage preferences'
+        },
+        preferencesModal: {
+          title: 'Manage cookie preferences',
+          acceptAllBtn: 'Accept all',
+          acceptNecessaryBtn: 'Reject all',
+          savePreferencesBtn: 'Save preferences',
+          closeIconLabel: 'Close'
+        }
+      }
+    }
+  }
 });
