945d67a5942d9c8963902880cc01abd808cb0055
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2026-02-07 14:27:00 -0500

    update

--- csv-to-json/convertCsvToJson.mjs
+++ csv-to-json/convertCsvToJson.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const csvFilePath = path.join(__dirname, "./Kaplan-Master-3 (2).csv");
+const csvFilePath = path.join(__dirname, "./Kaplan-Master-3 (1).csv");
 const jsonFilePath = path.join(__dirname, '../src/data/items.json')
 
 /*
